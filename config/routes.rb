56f1ad76cdd7bea50a01ef373450426669b21b51
uuushiro <yushiro.ma2ta2.21@gmail.com>
2016-08-29 00:43:34 +0900

    とりあえずいいね機能までつけてみた

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,26 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do
+      get :like_notes
+    end
+  end
+
+  # memberを使ってliking_usersのルーティングを定義してください
+  resources :notes, only: [:show, :create, :edit, :update, :destroy] do
+    member do
+      get :liking_users
+    end
+  end
+
+  post '/like/:note_id' => 'likes#like', as: 'like'
+  delete '/unlike/:note_id' => 'likes#unlike', as: 'unlike'
+
+  root 'home#top'
+  get '/about' => 'home#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
